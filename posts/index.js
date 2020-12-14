e58c01af563704e6ccad337106c80547def6d379
Selena Siri <selenasiri@gmail.com>
2020-12-13 19:20:13 -0500

    posts

--- posts/index.js
+++ posts/index.js
@@ -1,7 +1,27 @@
 import express from 'express';
+import bodyParser from 'body-parser'
+import { randomBytes } from 'crypto'
 
 const app = express()
+app.use(bodyParser.json());
 
+const posts = {};
+
+app.get('/posts', (req, res) => {
+  res.send(posts);
+});
+
+app.post('/posts', (req, res) => {
+  const id = randomBytes(4).toString('hex');
+  const { title } = req.body;
+
+  posts[id] = {
+    id,
+    title
+  };
+
+  res.status(201).send(posts[id]);
+});
 const PORT = 4000;
 
 app.listen(PORT, () => {
