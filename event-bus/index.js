9d7b68f842d60c4565055b95d9e4ce92b4264079
Selena Siri <selenasiri@gmail.com>
2020-12-15 12:49:22 -0500

    event-bus dockerfile

--- event-bus/index.js
+++ event-bus/index.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const axios = require('axios');
+import express from 'express';
+import bodyParser from 'body-parser'
+import axios from 'axios'
 
 const app = express();
 app.use(bodyParser.json());
